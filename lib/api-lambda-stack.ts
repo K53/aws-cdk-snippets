a3413c583c23b1679ffcd30febd2376133e3914b
keigo <devenv.keigo@gmail.com>
2021-12-05 03:13:02 +0900

    feat: default api-lambda

--- lib/api-lambda-stack.ts
+++ lib/api-lambda-stack.ts
@@ -1,16 +1,63 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 
 export class ApiLambdaStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // == const ==
+    const funcName = "cdksnippetFunc";
+    const apiName = "cdksnippetApi";
+    const apiStageName = "dev";
+    const apiPathName = "test";
+    const apiPath2Name = "test";
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ApiLambdaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Authorizer Lambda
+    // ToDo
+
+    // Resource Lambda
+    const myfunc = new lambda.Function(this, funcName, {
+      code: new lambda.AssetCode("src/cdksnippetFunc"),
+      handler: "index.handler",
+      runtime: lambda.Runtime.NODEJS_14_X
+    })
+    // IAM Managed Policy
+    myfunc.addToRolePolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: ["cloudfront:CreateInvalidation"], // Managed Policy
+      resources: ["*"]
+    }))
+
+    // API
+    const myapi = new apigw.RestApi(this, apiName, {
+      restApiName: apiName,
+      deployOptions: {
+        stageName: apiStageName,
+      },
+    })
+
+    // Path
+    const myapiPath = myapi.root.addResource(apiPathName);
+    const myapiPath2 = myapiPath.addResource(apiPath2Name);
+
+    // Method
+    myapiPath.addMethod("GET", new apigw.LambdaIntegration(myfunc), {
+      methodResponses: [
+        {
+          statusCode: "200",
+        }
+      ]
+    });
+    myapiPath2.addMethod("GET", new apigw.LambdaIntegration(myfunc), {
+      methodResponses: [
+        {
+          statusCode: "200",
+        }
+      ]
+    });
+  
   }
 }
