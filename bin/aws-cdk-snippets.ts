ec2f4299d3387fc61342871d630e499b8dc4b18c
keigo <devenv.keigo@gmail.com>
2021-12-12 15:47:28 +0900

    feat: add lambda to sqs stack

--- bin/aws-cdk-snippets.ts
+++ bin/aws-cdk-snippets.ts
@@ -1,31 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-// import { ApiLambdaWithBasicAuthStack } from '../lib/api-lambda-with-basic-auth-stack';
-// import { ApiLambdaWithCognitoStack } from '../lib/api-lambda-with-cognito-stack';
-// import { ApiLambdaStack } from '../lib/api-lambda-stack';
-// import { ApiLambdaCustomDomainStack } from '../lib/api-lambda-custom-domain-stack';
-// import { SqsLambdaTriggerStack } from '../lib/sqs-lambda-trigger-stack';
-// import { LambdaWithLayerStack } from '../lib/lambda-with-layer-stack';
-// import { DynamodbCRUDLambdaStack } from '../lib/dynamodb-crud-lambda-stack';
-// import { CloudFrontS3HostingStack } from '../lib/cloudfront-s3-hosting-stack';
-import { CloudFrontLambdaEdgeS3WithWafStack } from '../lib/CloudFrontLambdaEdgeS3WithWafStack/cloudfront-lambdaedge-s3-with-waf-stack';
-import { WafForCloudFrontStack } from '../lib/CloudFrontLambdaEdgeS3WithWafStack/waf-for-cloudfront-stack';
-interface Config {
-  account: string;
-}
-const config : Config = require("../secrets/accountInfo");
+import { LambdaToSqsStack } from '../lib/LambdaToSqsStack/lambda-to-sqs-stack';
 
 const app = new cdk.App();
-new WafForCloudFrontStack(app, 'WafForCloudFrontStack', {
-    env: {
-        account: config.account,
-        region: "us-east-1",
-    }
-})
-new CloudFrontLambdaEdgeS3WithWafStack(app, 'CloudFrontLambdaEdgeS3WithWafStack', {
-    env: {
-        account: config.account,
-        region: "ap-northeast-1",
-    }
-});
+new LambdaToSqsStack(app, 'LambdaToSqsStack');
