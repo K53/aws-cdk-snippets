644ba9a1316387832b52c4c58593d105380d9bd6
keigo <devenv.keigo@gmail.com>
2021-12-22 12:41:40 +0900

    chore: change api-lambda to hosting

--- bin/aws-cdk-snippets.ts
+++ bin/aws-cdk-snippets.ts
@@ -5,11 +5,9 @@ import { CodeCommitStack } from '../samples/WebApp/codecommit-stack';
 import { ApiLambdaWithCognitoStack } from '../samples/WebApp/api-lambda-with-cognito-stack';
 import { CloudFrontS3HostingStack } from '../samples/WebApp/cloudfront-s3-hosting-stack';
 import { PipelineStack } from '../samples/WebApp/pipeline-stack';
-import { ApiLambdaWithKeyStack } from '../lib/ApiLambdaWithKey/api-lambda-with-key-stack';
 
 const app = new cdk.App();
-// new CodeCommitStack(app, 'CodeCommitStack');
-// new CloudFrontS3HostingStack(app, 'CloudFrontS3HostingStack')
-// new ApiLambdaWithCognitoStack(app, 'ApiLambdaWithCognitoStack');
-// new PipelineStack(app, 'PipelineStack');
-new ApiLambdaWithKeyStack(app, 'ApiLambdaWithKeyStack');
+new CodeCommitStack(app, 'CodeCommitStack');
+new CloudFrontS3HostingStack(app, 'CloudFrontS3HostingStack')
+new ApiLambdaWithCognitoStack(app, 'ApiLambdaWithCognitoStack');
+new PipelineStack(app, 'PipelineStack');
